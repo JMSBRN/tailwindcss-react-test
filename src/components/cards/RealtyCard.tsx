0167a172d39aafe4b30906afec100b9b5a98761d
JMSBRN <zakhavai@gmail.com>
2024-03-08 11:38:50 +0300

    fix: fixed  footer card styles

--- src/components/cards/RealtyCard.tsx
+++ src/components/cards/RealtyCard.tsx
@@ -29,14 +29,14 @@ const RealtyCard: React.FC<RealtyCardType> = ({
           <p className="card__price">{price}</p>
           <p className="card__address">{address}</p>
         </div>
-        <div className="card__footer">
+        <div className="card__footer ">
           <div className="card__footer-item">
             <svg className="card__footer-icon h-6 w-6 text-gray-600 fill-current mr-3">
               <path d="M0 16L3 5V1a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v4l3 11v5a1 1 0 0 1-1 1v2h-1v-2H2v2H1v-2a1 1 0 0 1-1-1v-5zM19 5h1V1H4v4h1V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1h2V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1zm0 1v2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1V6h-2v2a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6H3.76L1.04 16h21.92L20.24 6H19zM1 17v4h22v-4H1zM6 4v4h4V4H6zm8 0v4h4V4h-4z"></path>
             </svg>
             <div>
               <p className="card__footer-text">
-                <span className="font-bold">{bedrooms}</span> Bedrooms
+                <span className="font-bold text-sm">{bedrooms}</span> Bedrooms
               </p>
             </div>
           </div>
@@ -49,7 +49,7 @@ const RealtyCard: React.FC<RealtyCardType> = ({
             </svg>
             <div>
               <p className="card__footer-text">
-                <span className="font-bold">{bathrooms}</span> Bathrooms
+                <span className="font-bold text-sm">{bathrooms}</span> Bathrooms
               </p>
             </div>
           </div>
